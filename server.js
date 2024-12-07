d6985aa60da30a4441af59fe5500e25946f55896
Sarp <sarpinmacbookprosu@unknown4eb304580470.home>
2024-12-07 22:09:59 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Dynamic route with parameter
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User ID: ${userId}`);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
