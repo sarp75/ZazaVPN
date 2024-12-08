803259ba30aff345a0f2a0f431f2b62b0b82b81c
prosarp1 <78538670+sarp75@users.noreply.github.com>
2024-12-08 11:25:18 +0300

    Push

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  // Render will automatically assign a port
 
 // Basic route
 app.get('/', (req, res) => {
-    res.send('Hello, Nigga!');
-});
-
-// Dynamic route with parameter
-app.get('/user/:id', (req, res) => {
-    const userId = req.params.id;
-    res.send(`User ID: ${userId}`);
+    res.send('ZazaVPN is ALIVE!');
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
